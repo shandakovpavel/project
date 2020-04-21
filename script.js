453b548962413d481c552545febfe69b4cf8b202
Pavel <shandakovpavel@gmail.com>
2020-04-21 22:14:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let asd = 2;
+let zxc = 3;
+
+
+console.log(4/0);
+console.log('aaa' * 1);
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone.name);
+
+let arr = [1,3,4,5,6];
+
+console.log(arr[2]);
+
+// alert('Hello world');
+// let answer = confirm('Are u here?');
+// console.log(answer);
+
+
+
+
